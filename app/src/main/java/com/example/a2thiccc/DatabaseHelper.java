d3c4e0bc0618921635f55c764acd6e341ecba6bb
Jun Cao <juncao97@gmail.com>
2019-10-25 13:37:38 -0400

    Edited DatabaseHelper class.

--- app/src/main/java/com/example/a2thiccc/DatabaseHelper.java
+++ app/src/main/java/com/example/a2thiccc/DatabaseHelper.java
@@ -10,12 +10,10 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "users.db";
     private static final String TABLE_NAME = "users";
     private static final String COLUMN_ID = "id";
-    private static final String COLUMN_NAME = "name";
-    private static final String COLUMN_EMAIL = "email";
     private static final String COLUMN_UNAME = "uname";
     private static final String COLUMN_PASS = "pass";
     private static final String TABLE_CREATE = "create table contacts (id integer primary key not null auto_increment, " +
-            "name text not null , email text not null , uname text not null , pass text not null);";
+            " uname text not null , pass text not null);";
     SQLiteDatabase db;
 
     public DatabaseHelper(Context context) {
